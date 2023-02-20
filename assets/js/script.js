6ba03a5d1e9869da87d17050b5505e36c4a22091
Travis Swift <travis.arthur.swift@gmail.com>
2023-02-19 18:44:14 -0600

    I made a circle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke();
