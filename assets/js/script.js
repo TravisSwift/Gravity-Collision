cc5b6d42ade9c822fbd0642a440986af35e731c2
Travis Swift <travis.arthur.swift@gmail.com>
2023-02-19 19:58:42 -0600

    created a circle with javascript in canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
-var canvas = document.getElementById("myCanvas");
+// creating a canvas object
+var canvas = document.getElementById("canvas");
+//Create a 2d drawing object  for the canvas object
 var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+// Calculate the clock radius, using the height of the canvas
+var radius = canvas.height / 2;
+// Remap the (0,0) position (of the drawing object) to the center of the canvas:
+ctx.translate(radius, radius);
+// Reduce the clock radius (to 90%) to draw the clock well inside the canvas
+radius = radius * 0.90
+drawClock();
+
+
+// Create a function to draw the clock:
+function drawClock() {
+  ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+  ctx.fillStyle = "white";
+  ctx.fill();
+}
