63c44646502bda1d0dee47ad6b2c36ade431f994
Travis Swift <travis.arthur.swift@gmail.com>
2023-03-17 22:14:01 -0500

    attached player movement to arrow keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,18 @@
  var myGameArea = {
      canvas : document.createElement("canvas"),
      start : function() {
-         this.canvas.width = 480;
-         this.canvas.height = 270;
-         this.context = this.canvas.getContext("2d");
-         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-         // Update the updateGameArea() function every 20th millisecond 
-         this.interval = setInterval(updateGameArea, 20);
-         window.addEventListener('keydown', function (e) {
-            myGameArea.key = false;
-         })
-        },
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+          myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+          myGameArea.key = false;
+        })
+      },
         clear : function() {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);    
      }
@@ -47,9 +49,15 @@
 // Game piece movement
 function updateGameArea() {
     myGameArea.clear();
-    myGamePiece.newPos();    
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();
     myGamePiece.update();
-}
+  }
 
 function moveup() {
     myGamePiece.speedY -= 1; 
